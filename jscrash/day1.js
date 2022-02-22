cd045635bba20772a9000eb2575fefa0e647d21e
ABIMS707 <adebayoabimbola9@gmail.com>
2022-02-22 19:14:13 +0100

    proof of concept init

--- jscrash/day1.js
+++ jscrash/day1.js
@@ -9,3 +9,31 @@ function average(arrayOfNumbers) {
 }
 
 average([1,4,5,6,7,8,])
+
+
+
+function meanOfMultipleArrays(allArrays){
+    let newArr =[];
+    for(let i = 0; i<allArrays.length; i++){
+        let avr = average(allArrays[i])
+        newArr.push(avr)
+    }
+    console.log(newArr)
+    console.log(average(newArr))
+
+}
+meanOfMultipleArrays([
+    [2,4],
+    [4,9],
+    [0,3],
+    [5,9]
+    [2,1],
+    [3,9],
+    [5,7],
+    
+
+
+])
+
+
+[[2,4,5],[1.5,6],[6,9,0]]
